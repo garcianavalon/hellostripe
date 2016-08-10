deab103735239162409c7769388e0fdce9c3c815
Enrique Garcia Navalon <garcianavalon@gmail.com>
2016-08-10 17:20:15 +0200

    submitting cards

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  res.render('index', {
+    stripeApiKey: process.env.STRIPE_API_KEY});
 });
 
 module.exports = router;
